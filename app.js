89db0fcb644ca154c46b300e185406e5fdef62a2
kevinxft <kevinxft@gmail.com>
2020-03-15 19:28:10 +0800

    :construction: 增加通用的admin接口

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const { mongoose } = require("./db")
+const ObjectId = mongoose.Types.ObjectId
 const Koa = require("koa")
 const app = new Koa()
 const router = require("koa-router")()
 const bodyparse = require("koa-bodyparser")
 const FastScanner = require("fastscan")
-const { Sensitive } = require("./schema")
+const MODEL = require("./schema")
 const cors = require("koa2-cors")
+const { v4: uuid } = require("uuid")
 
 require("dotenv").config()
 const PORT = process.env.PORT
@@ -14,61 +16,105 @@ app.use(bodyparse())
 let scanner = null
 
 const loadData = async () => {
-  const data = await Sensitive.find({}).exec()
+  const data = await MODEL["Sensitive"].find({}).exec()
   const words = data.map(item => item.word)
   scanner = new FastScanner(words)
   const count = words.length
   console.log(`数据加载完毕, 总共${count}条记录`)
   return count
 }
-
 loadData()
 
-
-router.get("/admin/words", async ctx => {
-  const { page = 1, pageSize = 20, ...rest } = ctx.query
-  const count = await Sensitive.countDocuments(rest)
-  const data = await Sensitive.find(rest)
-    .skip((Number(page) - 1) * Number(pageSize))
+const pagination = async (model, ctx) => {
+  const { current = 1, pageSize = 20, ...params } = ctx.query
+  const count = await model.countDocuments(params)
+  const data = await model
+    .find(params, null, { sort: [{ _id: -1 }] })
+    .skip((Number(current) - 1) * Number(pageSize))
     .limit(Number(pageSize))
     .exec()
-  ctx.body = {
+  return {
     count,
-    data,
+    data
+  }
+}
+
+router.get("/admin/:model", async ctx => {
+  try {
+    const modelName = ctx.params.model
+    ctx.body = await pagination(MODEL[modelName], ctx)
+  } catch (error) {
+    ctx.status = 400
+    ctx.body = error
   }
 })
 
-router.post("/admin/words", async ctx => {
-  ctx.body = "admin"
+router.post("/admin/:model", async ctx => {
+  try {
+    const modelName = ctx.params.model
+    const form = ctx.request.body
+    const res = await new MODEL[modelName](form).save((err, info) => {
+      if (err) {
+        return Promise.reject(err)
+      }
+      return Promise.resolve(info)
+    })
+    ctx.body = res
+  } catch (error) {
+    ctx.body = error
+  }
 })
 
-router.delete("/admin/words/:id", async ctx => {
-  await Sensitive.deleteOne({ _id: ctx.params.id})
-  ctx.body = `delete ${ctx.params.id}`
+router.delete("/admin/:model/:id", async ctx => {
+  const modelName = ctx.params.model
+  const res = await MODEL[modelName].deleteOne({ _id: ctx.params.id })
+  ctx.body = res
 })
 
-router.put("/admin/words", async ctx => {
-  ctx.body = "admin"
-})
-
-router.get("/admin/reload", async ctx => {
+router.get("/sensitive/reload", async ctx => {
   const count = await loadData()
   ctx.body = count
 })
 
+const validateAppID = async appID => {
+  const appModel = MODEL["App"]
+  const app = await appModel.findOne({ appID }).exec()
+  return app
+}
+
+const inertLog = async words => {
+  try {
+    const LogModel = MODEL["Log"]
+    if (words.length) {
+      new LogModel({ words }).save(err => {
+        if (err) {
+          return Promise.reject(err)
+        }
+        Promise.resolve()
+      })
+    }
+  } catch (error) {
+    return error
+  }
+}
+
 router.post("/", async ctx => {
-  const { text = "", options = { quick: true } } = ctx.request.body
+  const { text = "", options = {} } = ctx.request.body
+  const { quick = false, longest = false, ...rest } = options
   if (text.trim() === "") {
     ctx.response.status = 400
     ctx.body = {
       code: 400,
-      message: "text is required",
+      message: "text is required"
     }
     return
   }
-  const offWords = scanner.search(text, options)
+  const offWords = scanner.search(text, { quick, longest })
+  if (offWords.length) {
+    inertLog(offWords)
+  }
   ctx.body = {
-    data: offWords,
+    data: offWords
   }
 })
 
@@ -76,5 +122,5 @@ app.use(router.routes())
 app.use(router.allowedMethods())
 
 app.listen(PORT, () =>
-  console.log(`sensitive word detection server is running on port: ${PORT}`),
+  console.log(`sensitive word detection server is running on port: ${PORT}`)
 )
--- schema.js
+++ schema.js
@@ -1,15 +1,78 @@
 const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const { v4: uuid } = require("uuid")
 
-const Sensitive = mongoose.model("Sensitive", {
-  word: {
-    type: String,
-    index: true,
+const Sensitive = mongoose.model(
+  "Sensitive",
+  new Schema(
+    {
+      word: {
+        type: String,
+        index: true
+      },
+      tag: String
+    },
+    {
+      timestamps: true
+    }
+  )
+)
+
+const appSchema = new Schema(
+  {
+    name: {
+      type: String,
+      index: true
+    },
+    desc: {
+      type: String
+    },
+    appID: {
+      type: String
+    },
+    createAt: {
+      type: Date
+    }
   },
-  tag: String,
+  {
+    timestamps: true
+  }
+)
+
+appSchema.pre("save", function(next) {
+  console.log("save")
+  if (!this.appID) {
+    this.appID = uuid()
+  }
+  next()
 })
 
+const App = mongoose.model("App", appSchema)
+
+const Log = mongoose.model(
+  "Log",
+  new Schema(
+    {
+      appName: {
+        type: String,
+        index: true
+      },
+      appID: {
+        type: String,
+        index: true
+      },
+      words: {
+        type: Array
+      }
+    },
+    {
+      timestamps: true
+    }
+  )
+)
 
 module.exports = {
-  Sensitive
+  Sensitive,
+  App,
+  Log
 }
-
